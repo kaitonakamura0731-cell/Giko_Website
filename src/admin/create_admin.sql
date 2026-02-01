fe12c36a363db75ed836b7e7d0beb15d3c987eb4
zakiyama19980312 <zakiyama19980312@gmail.com>
2026-02-01 13:24:15 +0900

    サイト確認まで完了

---
+++ src/admin/create_admin.sql
@@ -0,0 +1,5 @@
+-- Create Default Admin User
+-- Username: admin
+-- Password: password
+INSERT INTO admin_users (username, password_hash) 
+VALUES ('admin', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi');
